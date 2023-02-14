254c83003111b81bf635e17f10c8fa0f29807bb0
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2023-02-14 23:33:43 +0530

    initial commit

---
+++ app/about/loading.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Loading({ error, reset }) {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,27 @@
+import Movie from "./Movie";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const response = await data.json();
+  //console.log(response);
+
+  return (
+    <main>
+      <div className="grid gap-16 grid-cols-fluid">
+        {response?.results.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
+    </main>
+  );
+}
+
+//https://api.themoviedb.org/3/movie/popular?api_key=
